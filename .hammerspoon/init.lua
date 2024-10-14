8508eb26d784c17402e1140e9746b8adf7a390ae
Rok Pergarec <rok@parabola.si>
2024-10-14 15:57:09 +0200

    disable hammerspoon app management, enable aerospace

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,13 @@
 -- Require modules
 require "src/config"
-require "src/apps"
 require "src/browser"
 require "src/windows"
 require "src/spectacle"
 require "src/bluetooth"
-require "src/screens"
 require "src/idasen"
 require "src/travel"
 require "src/caffeinate"
+
+-- Disable if AeroSpace is enabled
+--require "src/apps"
+--require "src/screens"
